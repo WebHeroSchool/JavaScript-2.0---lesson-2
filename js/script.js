e6a254e56507d6d843150185023ef12669079378
Tatiana Kotenko <sunderin@outlook.de>
2021-12-02 22:17:11 +0100

    js file added

---
+++ js/script.js
@@ -0,0 +1,23 @@
+question1 {
+  question: 
+  answer: 
+  correctAnswer:
+}
+
+question2 {
+  question:
+  answer:
+  correctAnswer:
+}
+
+question3 {
+  question:
+  answer:
+  correctAnswer:
+}
+
+question4 {:
+  question:
+  answer:
+  correctAnswer:
+}
